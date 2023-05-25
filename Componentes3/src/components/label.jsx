595b0846a2654b356353ee635caee68d8f57ce5c
Franco Pecci <fapeccisan@gmail.com>
2023-05-25 08:42:06 -0400

    s

--- Componentes3/src/components/label.jsx
+++ Componentes3/src/components/label.jsx
@@ -10,7 +10,7 @@ function Label({card}){
     }
 
     return (
-        <div className="Label"><h1 className="text" onClick={toggle}><span>Label</span>{showSubLabel ? "🢂" : "🢃"}</h1>
+        <div className="Label"><h1 className="text" ><span>Label</span><span onClick={toggle}>{showSubLabel ? "🢂" : "🢃"}</span></h1>
             <h2>{showSubLabel ? "" : card.description}</h2>
         </div>
     )
