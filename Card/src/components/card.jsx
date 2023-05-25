9c09676ee347fcda0294add84f8f668d43779e03
Franco Pecci <fapeccisan@gmail.com>
2023-05-25 11:28:14 -0400

    n

---
+++ Card/src/components/card.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Card.css"
+import { useState } from "react";
+
+function Card({}){
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Card
