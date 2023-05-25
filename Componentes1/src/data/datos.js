5b08311e5561f292d457f8cb9f46d2bbe84306d2
Franco Pecci <fapeccisan@gmail.com>
2023-05-25 09:40:18 -0400

    a

--- Componentes1/src/data/datos.js
+++ Componentes1/src/data/datos.js
@@ -1,22 +1,26 @@
 const Mcdonalds = [
     {
         name: "Combo Cuarto de Libra",
-        description: "Doble queso derretido, …la compañía perfecta para que la hamburguesa no se sienta sola. Simple, pero funciona. Acompañado de papas y gaseosa."
+        description: "Doble queso derretido, …la compañía perfecta para que la hamburguesa no se sienta sola. Simple, pero funciona. Acompañado de papas y gaseosa.", 
+        imagen: "Cuarto_de_libra.png"
     },
 
     {
         name: "Combo Doble Cuarto de Libra",
-        description: "El doble de sabor. A ver… empecemos con un cuarto de libra de pura carne con los quesos que lo acompañan. Hasta ahí, muy bien. Sólo que encima de todo esto, agregamos otro cuarto de libra, igual al anterior. ¡Mucho Mejor! Acompañado con papas y gaseosa."
+        description: "El doble de sabor. A ver… empecemos con un cuarto de libra de pura carne con los quesos que lo acompañan. Hasta ahí, muy bien. Sólo que encima de todo esto, agregamos otro cuarto de libra, igual al anterior. ¡Mucho Mejor! Acompañado con papas y gaseosa.",
+        imagen: "Doble_cuarto_de_libra.png"
     },
 
     {
         name: "Combo Premium Bacon",
-        description: "Elaborada con carne 100% premium. Esta carne se destaca por su irresistible sabor. Premium Bacon, grande, tentadora, inigualable, la mejor opción para quienes tienen un paladar exquisito. Acompañados con papas y gaseosa."
+        description: "Elaborada con carne 100% premium. Esta carne se destaca por su irresistible sabor. Premium Bacon, grande, tentadora, inigualable, la mejor opción para quienes tienen un paladar exquisito. Acompañados con papas y gaseosa.",
+        imagen: "Premium_bacon.png"
     },
 
     {
         name: "Combo Big Mac",
-        description: "¿Qué es lo que hace al Big Mac único? Quizás sean las dos hamburguesas de pura carne con salsa y queso derretido, el toque de cebolla, el crocante del pepino. Bueno, en realidad quizás sea solo que es el más alto… Acompañado de papas y gaseosa."
+        description: "¿Qué es lo que hace al Big Mac único? Quizás sean las dos hamburguesas de pura carne con salsa y queso derretido, el toque de cebolla, el crocante del pepino. Bueno, en realidad quizás sea solo que es el más alto… Acompañado de papas y gaseosa.",
+        imagen: "Big_mac.png"
     },
 ]
 
