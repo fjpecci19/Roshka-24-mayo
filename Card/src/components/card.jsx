df969f21bc4dd2a0f5fb77d0837d36ed2a84d5e9
Franco Pecci <fapeccisan@gmail.com>
2023-05-25 15:49:13 -0400

    a

--- Card/src/components/card.jsx
+++ Card/src/components/card.jsx
@@ -9,14 +9,15 @@ function Card({}){
             <p className="text">GUNS N ROSES</p>
             <p className="text">Guns N' Roses es una banda estadounidense de hard rock formada en Hollywood, Los Ángeles en la zona de Sunset Strip, en 1985. El grupo musical fue fundado por el vocalista Axl Rose y el guitarrista Izzy Stradlin.</p>
             <div className="button-container">
-                <button className="button">Canciones</button>
-                <button className="button">Álbumes</button>
-                <button className="button">Integrantes</button>
+                <button className="button"><Link to="https://es.wikipedia.org/wiki/Guns_N%27_Roses">Biografía</Link></button>
+                <button className="button"><Link to="https://es.wikipedia.org/wiki/Anexo:Discograf%C3%ADa_de_Guns_N%27_Roses">Álbumes</Link></button>
+                <button className="button"><Link to="https://es.wikipedia.org/wiki/Guns_N%27_Roses">Integrantes</Link></button>
             </div>
             <div>
                 <h2>1985</h2>
                 <h2><Link to="https://www.youtube.com/watch?v=Rbm6GXllBiw">Paradise City: </Link></h2>
                 <h2><Link to="https://www.youtube.com/watch?v=o1tj2zJ2Wvg">Welcome to the jungle: </Link></h2>
+                <h2><Link to="https://www.youtube.com/watch?v=8SbUC-UaAxE">November Rain: </Link></h2>
             </div>
         </div>
     )
