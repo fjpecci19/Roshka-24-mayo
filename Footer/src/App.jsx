ae00b9b52dafee728a4f24193dfa6ae041a87c10
Franco Pecci <fapeccisan@gmail.com>
2023-05-26 09:06:44 -0400

    a

---
+++ Footer/src/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <div className="column">
+        
+      </div>
+      <div>
+        Products<br /><br />
+        <div className="coso">ICM</div><br />
+        <div className="coso">DMS</div><br />
+        <div className="coso">CRM</div>
+      </div>
+      <div>
+        Resources<br /><br />
+        <div className="coso">Case Studies</div><br />
+        <div className="coso">FAQ'S</div><br />
+        <div className="coso">Newsletter</div><br />
+        <div className="coso">Release Notes</div><br />
+        <div className="coso">Developer's Guide</div>
+      </div>
+      <div>
+        Company<br /><br />
+        <div className="coso">About</div><br />
+        <div className="coso">Our Experts</div><br />
+        <div className="coso">Terms & Conditions</div><br />
+        <div className="coso">Schedule a Demo</div><br />
+        <div className="coso">Privacy Policy</div>
+      </div>
+      <div>
+        Contact us <br /><br /><br />
+        <img src="images/Facebook.jpg"/> <img src="images/Linkedin.jpg"/> <img src="images/Twitter.jpg"/>
+      </div>
+    </div>
+  )
+}
+
+export default App
